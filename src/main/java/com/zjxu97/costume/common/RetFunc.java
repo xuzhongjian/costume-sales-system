08f8a6725ef46c9ac11e3b3e2c1242c19f2dc309
xuzhongjian <xuzhongjian@xiaomi.com>
2019-12-08 17:15:20 +0800

    finsh log

---
+++ src/main/java/com/zjxu97/costume/common/RetFunc.java
@@ -0,0 +1,51 @@
+package com.zjxu97.costume.common;
+
+import com.baomidou.mybatisplus.extension.api.R;
+
+/**
+ * date    2019-08-23
+ * time    12:33
+ *
+ * @author thisxzj - 中建
+ */
+
+
+public class RetFunc {
+
+    /**
+     * 执行成功
+     */
+    private static final String SUCCESS_MSG = "执行成功";
+
+    /**
+     * 执行失败
+     */
+    private static final String FAILURE_MSG = "执行失败:";
+
+    /**
+     * 执行成功
+     */
+    private static final Long SUCCESS_CODE = 1000L;
+
+    /**
+     * 执行失败
+     */
+    private static final Long FAILURE_CODE = 2000L;
+
+
+    public static <T> R<T> success(T data) {
+        R<T> r = new R<>();
+        r.setCode(SUCCESS_CODE);
+        r.setData(data);
+        r.setMsg(SUCCESS_MSG);
+        return r;
+    }
+
+    public static <T> R<T> failure(Exception e) {
+        R<T> r = new R<>();
+        r.setCode(FAILURE_CODE);
+        r.setData(null);
+        r.setMsg(FAILURE_MSG + e.toString());
+        return r;
+    }
+}
