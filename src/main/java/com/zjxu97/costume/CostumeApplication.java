cc91be53a4e1a89ec4252f815807baa8dfc7d00c
xuzhongjian <xuzhongjian@xiaomi.com>
2020-01-18 16:18:04 +0800

    同源策略

--- src/main/java/com/zjxu97/costume/CostumeApplication.java
+++ src/main/java/com/zjxu97/costume/CostumeApplication.java
@@ -3,7 +3,11 @@ package com.zjxu97.costume;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author thisxzj
@@ -14,6 +18,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class CostumeApplication {
     public static void main(String[] args) {
         SpringApplication.run(CostumeApplication.class, args);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
     }
 
 }
