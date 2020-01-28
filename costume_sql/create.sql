4178cdf725941f17386c61234083281b825b5d2b
xuzhongjian <xuzhongjian@xiaomi.com>
2020-01-28 16:45:57 +0800

    commit

--- costume_sql/create.sql
+++ costume_sql/create.sql
@@ -1,125 +1,174 @@
-CREATE TABLE `area`
+-- auto-generated definition
+create table area
 (
-    `id`         int(11) unsigned NOT NULL COMMENT '自增id',
-    `is_deleted` tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`  datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `area_name`  varchar(32)      NOT NULL COMMENT '大区域名称'
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='大区域名称';
+    id         int(11) unsigned                   not null comment '自增id'
+        primary key,
+    is_deleted tinyint  default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    area_name  varchar(32)                        not null comment '大区域名称'
+) DEFAULT CHARACTER SET = utf8,comment '大区域名称';
 
-CREATE TABLE `city`
-(
-    `id`          int(11) unsigned NOT NULL COMMENT '自增id',
-    `is_deleted`  tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`   datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`   datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `city_name`   varchar(32)      NOT NULL COMMENT '市名称',
-    `province_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '省级id',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='市级单位';
 
-CREATE TABLE `commodity_stock`
+-- auto-generated definition
+create table city
 (
-    `id`         int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `is_deleted` tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`  datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `store_id`   int(11) unsigned NOT NULL DEFAULT '0' COMMENT '店铺id',
-    `item_id`    int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品id',
-    `amount`     int(11)          NOT NULL DEFAULT '0' COMMENT '剩余库存',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='库存记录';
+    id          int(11) unsigned                           not null comment '自增id'
+        primary key,
+    is_deleted  tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at   datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at   datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    city_name   varchar(32)                                not null comment '市名称',
+    province_id int(11) unsigned default 0                 not null comment '省级id'
+) DEFAULT CHARACTER SET = utf8, comment '市级单位';
 
-CREATE TABLE `costume_type`
+-- auto-generated definition
+create table district
 (
-    `id`         int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `is_deleted` tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`  datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `type_name`  varchar(64)      NOT NULL COMMENT '类型名称',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='服装类型';
+    id            int(11) unsigned                           not null comment '自增id'
+        primary key,
+    is_deleted    tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at     datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at     datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    district_name varchar(32)                                not null comment '区县名称',
+    city_id       int(11) unsigned default 0                 not null comment '市级id'
+) DEFAULT CHARACTER SET = utf8, comment '区县单位';
 
-CREATE TABLE `district`
-(
-    `id`            int(11) unsigned NOT NULL COMMENT '自增id',
-    `is_deleted`    tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`     datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`     datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `district_name` varchar(32)      NOT NULL COMMENT '区县名称',
-    `city_id`       int(11) unsigned NOT NULL DEFAULT '0' COMMENT '市级id',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='区县单位';
 
-CREATE TABLE `item`
+-- auto-generated definition
+create table item
 (
-    `id`         int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `is_deleted` tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`  datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `item_name`  varchar(64)      NOT NULL COMMENT '名称',
-    `costume_id` int(11) unsigned NOT NULL COMMENT '服装类别的id',
-    `sex`        tinyint(4)                DEFAULT NULL COMMENT '性别:男1|女0|null无性别',
-    `item_size`  tinyint(4)                DEFAULT NULL COMMENT '服装大小:SML|etc',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='服装列表';
+    id           int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted   tinyint  default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at    datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    item_name    varchar(64)                        not null comment '名称',
+    item_type_id int(11) unsigned                   not null comment '服装类别的id'
+) DEFAULT CHARACTER SET = utf8, comment '服装列表';
 
-CREATE TABLE `province`
-(
-    `id`            int(11) unsigned NOT NULL COMMENT '自增id',
-    `is_deleted`    tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`     datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`     datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `province_name` varchar(32)      NOT NULL COMMENT '省的名称',
-    `area_id`       int(11) unsigned NOT NULL DEFAULT '0' COMMENT '区域id',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='省级单位';
 
-CREATE TABLE `sale_record`
+-- auto-generated definition
+create table item_detail
 (
-    `id`         int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `is_deleted` tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`  datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `store_id`   int(11) unsigned NOT NULL DEFAULT '0' COMMENT '店铺id',
-    `item_id`    int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品id',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='销售记录';
+    id           int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted   tinyint  default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at    datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    item_id      int(11) unsigned                   not null comment '款式id',
+    item_size_id int                                null comment '服装大小',
+    price        int      default 0                 not null comment '价格'
+) DEFAULT CHARACTER SET = utf8, comment '鞋服具体';
 
-CREATE TABLE `store`
-(
-    `id`            int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `is_deleted`    tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`     datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`     datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `district_id`   int(11) unsigned NOT NULL DEFAULT '0' COMMENT '区县的id',
-    `store_name`    varchar(64)      NOT NULL DEFAULT '服装店' COMMENT '店铺的名称',
-    `store_address` varchar(64)      NOT NULL DEFAULT '无' COMMENT '地址',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 8192
-  DEFAULT CHARSET = utf8 COMMENT ='商店列表';
-ALTER TABLE `costume`.`item`
-    CHANGE COLUMN `item_size` `item_size` INT(11) NULL DEFAULT NULL COMMENT '服装大小:SML|etc';
 
-CREATE TABLE `users`
+-- auto-generated definition
+create table item_size
 (
-    `id`         int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `is_deleted` tinyint(4)       NOT NULL DEFAULT '0' COMMENT '是否删除,1删除，0未删除',
-    `create_at`  datetime                  DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_at`  datetime         NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-    `user_name`  varchar(32)      NOT NULL COMMENT '用户名称',
-    `nick_name`  varchar(32)      NOT NULL DEFAULT '新员工' COMMENT '昵称',
-    `password`   varchar(64)      NOT NULL DEFAULT '25d55ad283aa400af464c76d713c07ad' COMMENT '12345678的32位md5',
-    `store_id`   int(11) unsigned NOT NULL DEFAULT '0' COMMENT '所属的店铺id',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8 COMMENT ='用户表';
+    id           int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted   tinyint  default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at    datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    display_name varchar(64)                        not null comment '类型名称'
+) DEFAULT CHARACTER SET = utf8, comment '鞋服大小';
+
+
+-- auto-generated definition
+create table item_type
+(
+    id         int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted tinyint  default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    sex        tinyint                            null comment '性别:男1|女0|null无性别',
+    type_name  varchar(64)                        not null comment '类型名称:鞋、衣服'
+) DEFAULT CHARACTER SET = utf8, comment '服装类型';
+
+
+-- auto-generated definition
+create table province
+(
+    id            int(11) unsigned                           not null comment '自增id'
+        primary key,
+    is_deleted    tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at     datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at     datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    province_name varchar(32)                                not null comment '省的名称',
+    area_id       int(11) unsigned default 0                 not null comment '区域id'
+) DEFAULT CHARACTER SET = utf8, comment '省级单位';
+
+
+-- auto-generated definition
+create table sale_record
+(
+    id             int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted     tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at      datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at      datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    store_id       int(11) unsigned default 0                 not null comment '店铺id',
+    item_detail_id int(11) unsigned default 0                 not null comment '商品id',
+    sale_type      tinyint          default 1                 null comment '1售出|2退回',
+    age            int              default 0                 not null comment '购买者年龄'
+) DEFAULT CHARACTER SET = utf8, comment '销售记录';
+
+
+-- auto-generated definition
+create table stock
+(
+    id             int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted     tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at      datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at      datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    store_id       int(11) unsigned default 0                 not null comment '店铺id',
+    item_detail_id int(11) unsigned default 0                 not null comment '商品id',
+    amount         int              default 0                 not null comment '剩余库存'
+) DEFAULT CHARACTER SET = utf8, comment '库存信息';
+
+
+-- auto-generated definition
+create table stock_record
+(
+    id             int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted     tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at      datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at      datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    store_id       int(11) unsigned default 0                 not null comment '店铺id',
+    item_detail_id int(11) unsigned default 0                 not null comment '商品id',
+    inout_type     tinyint          default 0                 not null comment '出入类型',
+    amount         int              default 0                 not null comment '数量'
+) DEFAULT CHARACTER SET = utf8, comment '库存出入信息';
+
+-- auto-generated definition
+create table store
+(
+    id            int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted    tinyint          default 0                 not null comment '是否删除,1删除，0未删除',
+    create_at     datetime         default CURRENT_TIMESTAMP null comment '创建时间',
+    update_at     datetime         default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '修改时间',
+    district_id   int(11) unsigned default 0                 not null comment '区县的id',
+    store_name    varchar(64)      default '服装店'             not null comment '店铺的名称',
+    store_address varchar(64)      default '无'               not null comment '地址'
+) DEFAULT CHARACTER SET = utf8, comment '商店列表';
+
+
+-- auto-generated definition
+create table users
+(
+    id         int(11) unsigned auto_increment comment '自增id'
+        primary key,
+    is_deleted tinyint          default 0                                  not null comment '是否删除,1删除，0未删除',
+    create_at  datetime         default CURRENT_TIMESTAMP                  null comment '创建时间',
+    update_at  datetime         default CURRENT_TIMESTAMP                  not null on update CURRENT_TIMESTAMP comment '修改时间',
+    user_id    varchar(32)                                                 not null comment '用户名称',
+    nick_name  varchar(32)      default '新员工'                              not null comment '昵称',
+    password   varchar(64)      default '25d55ad283aa400af464c76d713c07ad' not null comment '12345678的32位md5',
+    store_id   int(11) unsigned default 0                                  not null comment '所属的店铺id',
+    constraint users_user_id_uindex
+        unique (user_id)
+) DEFAULT CHARACTER SET = utf8, comment '用户表';
