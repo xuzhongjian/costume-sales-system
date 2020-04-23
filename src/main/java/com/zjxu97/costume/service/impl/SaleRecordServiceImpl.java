33267d24703cfa45beca1919cffd3beb51099ed6
xuzhongjian <xuzhongjian@xiaomi.com>
2020-04-23 18:59:12 +0800

    name

--- src/main/java/com/zjxu97/costume/service/impl/SaleRecordServiceImpl.java
+++ src/main/java/com/zjxu97/costume/service/impl/SaleRecordServiceImpl.java
@@ -2,15 +2,11 @@ package com.zjxu97.costume.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.zjxu97.costume.commons.Common;
-import com.zjxu97.costume.commons.Control;
-import com.zjxu97.costume.commons.CostumeConstants;
-import com.zjxu97.costume.commons.DataElement;
+import com.zjxu97.costume.commons.*;
 import com.zjxu97.costume.mapper.SaleRecordMapper;
 import com.zjxu97.costume.model.entity.ItemSize;
 import com.zjxu97.costume.model.entity.SaleRecord;
-import com.zjxu97.costume.service.ItemSizeService;
-import com.zjxu97.costume.service.SaleRecordService;
+import com.zjxu97.costume.service.*;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +14,7 @@ import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author zjxu97
@@ -29,28 +26,87 @@ public class SaleRecordServiceImpl extends ServiceImpl<SaleRecordMapper, SaleRec
     @Resource
     private ItemSizeService itemSizeService;
 
+    @Resource
+    private AreaService areaService;
+
+    @Resource
+    private CityService cityService;
+
+    @Resource
+    private ProvinceService provinceService;
+
+    @Resource
+    private DistrictService districtService;
+
+
+    @Resource
+    private ItemTypeService itemTypeService;
+
     @Override
     public List<DataElement> getDataList(Control control, String from, String to, String xType, String xValue, String yValue) {
         switch (xType) {
             case CostumeConstants.DATE:
                 return this.getBaseMapper().getDateData(control, from, to, xValue, yValue);
             case CostumeConstants.LOCATION:
-                return this.getBaseMapper().getLocationData(control, from, to, xValue, yValue);
+                List<DataElement> locationData = this.getBaseMapper().getLocationData(control, from, to, xValue, yValue);
+                List<Integer> idList = locationData.stream().map(DataElement::getKey).map(Integer::parseInt).collect(Collectors.toList());
+                Map<String, String> locationMap = new HashMap<>();
+                switch (xValue) {
+                    case LocationTypeConstants.AREA:
+                        areaService.listByIds(idList).forEach(area -> locationMap.put(String.valueOf(area.getId()), area.getAreaName()));
+                        break;
+                    case LocationTypeConstants.CITY:
+                        cityService.listByIds(idList).forEach(city -> locationMap.put(String.valueOf(city.getId()), city.getCityName()));
+                        break;
+                    case LocationTypeConstants.PROVINCE:
+                        provinceService.listByIds(idList).forEach(province -> locationMap.put(String.valueOf(province.getId()), province.getProvinceName()));
+                        break;
+                    case LocationTypeConstants.DISTRICT:
+                        districtService.listByIds(idList).forEach(district -> locationMap.put(String.valueOf(district.getId()), district.getDistrictName()));
+                        break;
+                }
+                for (DataElement size : locationData) {
+                    String key = size.getKey();
+                    if (Strings.isNotBlank(key)) {
+                        size.setKey(locationMap.get(key.trim()));
+                    }
+                }
+                return locationData;
             case CostumeConstants.ITEM_TYPE:
-                return this.getBaseMapper().getItemTypeData(control, from, to, yValue);
+                List<DataElement> itemTypeData = this.getBaseMapper().getItemTypeData(control, from, to, yValue);
+                Map<String, String> itemTypeMap = new HashMap<>();
+                itemTypeService.listByIds(itemTypeData.stream().map(DataElement::getKey).map(Integer::parseInt).collect(Collectors.toList()))
+                        .forEach(itemType -> itemTypeMap.put(String.valueOf(itemType.getId()), itemType.getTypeName()));
+                for (DataElement size : itemTypeData) {
+                    String key = size.getKey();
+                    if (Strings.isNotBlank(key)) {
+                        size.setKey(itemTypeMap.get(key.trim()));
+                    }
+                }
+                return itemTypeData;
             case CostumeConstants.ITEM:
-                return this.getBaseMapper().getItemData(control, from, to, yValue);
+                List<DataElement> itemData = this.getBaseMapper().getItemData(control, from, to, yValue);
+                Map<String, String> itemMap = new HashMap<>();
+                itemTypeService.listByIds(itemData.stream().map(DataElement::getKey).map(Integer::parseInt).collect(Collectors.toList()))
+                        .forEach(itemType -> itemMap.put(String.valueOf(itemType.getId()), itemType.getTypeName()));
+                for (DataElement size : itemData) {
+                    String key = size.getKey();
+                    if (Strings.isNotBlank(key)) {
+                        size.setKey(itemMap.get(key.trim()));
+                    }
+                }
+                return itemData;
             case CostumeConstants.SIZE:
                 List<ItemSize> list = itemSizeService.list();
                 List<DataElement> sizeData = this.getBaseMapper().getSizeData(control, from, to, yValue);
-                Map<String, String> map = new HashMap<>();
+                Map<String, String> sizeMap = new HashMap<>();
                 for (ItemSize size : list) {
-                    map.put(String.valueOf(size.getId()), size.getDisplayName());
+                    sizeMap.put(String.valueOf(size.getId()), size.getDisplayName());
                 }
                 for (DataElement size : sizeData) {
                     String key = size.getKey();
                     if (Strings.isNotBlank(key)) {
-                        size.setKey(map.get(key.trim()));
+                        size.setKey(sizeMap.get(key.trim()));
                     }
                 }
                 return sizeData;
